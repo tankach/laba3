e2a7b0f87b9a1ffc3b315667bf6a5dd16f4c766d
tankach <126239422+tankach@users.noreply.github.com>
2023-11-29 20:12:44 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumdu.edu.ua.webstudent;
+
+/**
+ *
+ * @author tanka
+ */
+public class Student {
+    private String name;
+    private String surname;
+    private String email;
+
+
+    public Student() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
